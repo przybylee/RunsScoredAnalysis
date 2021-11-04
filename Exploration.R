8e9c10ee79a058a58493453e07e5f803dcf57c85
Lee Przybylski <przybylskily@gmail.com>
2021-11-04 12:26:14 -0500

    Added mlb odds data

--- Exploration.R
+++ Exploration.R
@@ -1,8 +1,24 @@
 #Lee Przybylski
 #9/26/2021
+library(ggplot2)
 
 #Data Exploration with runs scored data
-
+options(stringsAsFactors = FALSE)
 games <- read.csv("MLB2019.csv")
 head(games)
+games$park <- paste(games$Home, "park", sep = "")
 summary(games)
+ggplot(games, aes(x= Home, y = Runs_Home)) + geom_boxplot()
+
+#Model in home team runs 
+pois1 <- glm(Runs_Home ~ Home + Away, data = games, family = poisson(link = "log"))
+summary(pois1)
+
+1-pchisq(deviance(pois1), 2370)
+#This shows there is evidence of overdispersion
+
+#Add park as a predictor
+pois2 <- glm(Runs_Home ~ Home + Away + park, data = games, family = poisson(link = "log"))
+summary(pois2)
+
+#We need to reshape the pdata
