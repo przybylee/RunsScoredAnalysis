6c985e389bdd3061e5777dc5d963aaf683a1be37
Lee Przybylski <przybylskily@gmail.com>
2021-11-16 21:54:06 -0600

    Worked out prediction function for poiss3

--- Code/FitModel/MainModel.R
+++ Code/FitModel/MainModel.R
@@ -1,7 +1,8 @@
 #Lee Przybylski 
 #11-11-2021
 
-#Fit our favorite glmer model and extract estimates for the pitcher
+#Fit our 3 favorite glmer models
+#Demonstrate prediction functions
 source("Code/DataFormatting.R")
 library(lme4)
 
@@ -13,15 +14,14 @@ df$gameID <- as.factor(df$gameID)
 df$home <- ifelse(df$Team == df$Venue, 1, 0)
 head(df)
 
-#Fit the model, takes about 23 minutes
+#Naive Model
 t0 <- Sys.time()
-poiss7 <- glmer(Final ~ Team + Opp + Venue + (1|OppPitcher) + (1|gameID),
-                data = df, family = poisson(link= "log"), 
-                control=glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e5)))
+poiss1 <- glm(Final ~ Team + Opp + Venue, data = df, family = poisson(link= "log"))
 tf <- Sys.time()
 print(tf - t0)
+#Fits in less than a second
 
-#Same thing with homefield advantage
+#Fixed effects for teams and venue
 t0 <- Sys.time()
 poiss7.5 <- glmer(Final ~ Team + Opp + Venue + home + (1|OppPitcher) + (1|gameID),
                   data = df, family = poisson(link= "log"), 
@@ -30,28 +30,64 @@ tf <- Sys.time()
 print(tf - t0)
 #Takes about 26 minutes to fit
 
-anova(poiss7,poiss7.5)
-#using a likelihood ratio test, gives a p-value of 0.006, home effects seem significant.
-fixef(poiss7.5)
+#All random except home
+t0 <- Sys.time()
+poiss3 <- glmer(Final ~ home + (1| Team) + (1| Opp) + (1|Venue) + (1|OppPitcher) 
+                + (1|gameID) + (1| obsID), data = df, family = poisson())
+tf <- Sys.time()
+print(tf - t0) #fits in 25 seconds
+
+summary(poiss3)
+
 
 head(df)
-#add predictions with all info
-df1 <- df
-yhat1 <- predict(poiss7.5, type = "response")
-df1$pred1 <- yhat1
-head(df1)
 
-#Drop game number and make predictions
-df2 <- df
-keep <- names(df2)[names(df2) != "OppPitcher"]
-df2 <- df2[,keep] 
+#Choose a game to predicton
+sampledf <- df[df$gameID == 150,]
+
+#Try to understand how predict works with poiss3
+sampledf$pred1 <- predict(poiss3, newdata = sampledf, type = "response")
+sampledf
+#intercept
+mu <- unlist(fixef(poiss3)[1])
+chi <- unlist(fixef(poiss3)[2])
+geff <- ranef(poiss3)$gameID[150,1]
+v <- ranef(poiss3)$Venue["LAD", 1]
+
+#For COL 
+eff <- ranef(poiss3)$obsID["COL150", 1]
+b <- ranef(poiss3)$Team["COL", 1]
+f <- ranef(poiss3)$Opp["LAD", 1]
+p <- ranef(poiss3)$OppPitcher["TBAUER", 1]
+lambda <- mu  + geff + v + eff + b + f + p
+exp(lambda)
+#For LAD 
+eff <- ranef(poiss3)$obsID["LAD150", 1]
+b <- ranef(poiss3)$Team["LAD", 1]
+f <- ranef(poiss3)$Opp["COL", 1]
+p <- ranef(poiss3)$OppPitcher["ASENZATEL", 1]
+lambda <- mu + chi + geff + v + eff + b + f + p
+exp(lambda)
+
+#If we want to predict without gameID and obsID
 #Predictions with conditioning only on the pitcher
-yhat2 <- predict(poiss7.5, re.form = ~ (1|OppPitcher), type = "response")
-df1$pred2 <- yhat2
-head(df1)
-#predictions with no conditioning
-yhat3 <- predict(poiss7.5, re.form = ~ 0, type = "response")
-df1$pred3 <- yhat3
-head(df1)
+yhat2 <- predict(poiss3, re.form = ~ (1| Team) + (1| Opp) + (1|Venue)
+                 + (1|OppPitcher), newdata = sampledf, type = "response")
+sampledf$pred2 <- yhat2
+sampledf
+#Predictions by hand
+#For COL 
+b <- ranef(poiss3)$Team["COL", 1]
+f <- ranef(poiss3)$Opp["LAD", 1]
+p <- ranef(poiss3)$OppPitcher["TBAUER", 1]
+lambda <- mu  + v + b + f + p
+exp(lambda)
+#For LAD 
+b <- ranef(poiss3)$Team["LAD", 1]
+f <- ranef(poiss3)$Opp["COL", 1]
+p <- ranef(poiss3)$OppPitcher["ASENZATEL", 1]
+lambda <- mu + chi + v + b + f + p
+exp(lambda)
+#Everything matches, Colorado got better when we removed the obsID effect, 
+#Which suggests they had a significantly below average game
 
-#When we bet, we must disregard effects from gameID and 
